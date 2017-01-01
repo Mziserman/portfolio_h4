16b4cab53dd6ab0948e4fc2dc126ef583b112ca0
Ziserman <martinziserman@gmail.com>
2017-01-01 18:26:19 +0100

    add admin and authentication

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,11 @@
 Rails.application.routes.draw do
+  devise_for :users
+  namespace :admin do
+    resources :projects
+
+    root to: "projects#index"
+  end
+
   get 'home/index'
 
   root controller: :home, action: :index
