4718d5ef805b8e99f7f6a502a1032cb549296bc6
Ziserman <martinziserman@gmail.com>
2017-01-07 16:32:23 +0100

    assets

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -8,6 +8,7 @@ class HomeController < ApplicationController
     @offer = Offer.new(params.require(:offer).permit(:title,
       :entreprise, :url, :message))
     if @offer.save
+      flash[:success] = "Thank you for the offer. I will get in touch with you as soon as possible."
       redirect_to action: :index
     else
       puts @offer.errors.inspect
--- config/routes.rb
+++ config/routes.rb
@@ -1,30 +1,15 @@
 Rails.application.routes.draw do
   namespace :admin do
     resources :users
-resources :domains
-resources :offers
-resources :projects
-resources :skills
-
-    root to: "users#index"
-  end
-
-  namespace :admin do
-    resources :users
     resources :domains
+    resources :offers
     resources :projects
     resources :skills
+
     root to: "users#index"
   end
 
   devise_for :users
-  namespace :admin do
-    resources :projects
-
-    root to: "projects#index"
-  end
-
-  get 'home/index'
   post '/create_offer' => 'home#create_offer'
 
   root controller: :home, action: :index
